03ba0d7a571e52f99492d43b54cd9fa849028817
Hadi Asghari <hadi.asgharime@gmail.com>
2023-06-10 08:34:58 +0200

    Update adding_mptcp_variants.sh

--- adding_mptcp_variants.sh
+++ adding_mptcp_variants.sh
@@ -2,28 +2,28 @@
 
 #lia
 sudo modprobe -v mptcp_coupled
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_coupled.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_coupled.ko
 
 #olia
 sudo modprobe -v mptcp_olia
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_olia.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_olia.ko
 
 
 #balia
 sudo modprobe -v mptcp_balia
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_balia.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_balia.ko
 
 #wvegas
 sudo modprobe -v mptcp_wvegas
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_wvegas.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_wvegas.ko
 
 #binder
 sudo modprobe -v mptcp_binder
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_binder.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_binder.ko
 
 #blest
 sudo modprobe -v mptcp_blest
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_blest.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_blest.ko
 
 #ECF
 sudo modprobe -v mptcp_ecf
@@ -31,19 +31,19 @@ sudo modprobe -v mptcp_ecf
 
 #ndiffports
 sudo modprobe -v mptcp_ndiffports
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_ecf.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_ecf.ko
 
 #netlink
 sudo modprobe -v mptcp_netlink
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_netlink.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_netlink.ko
 
 #redundant
 sudo modprobe -v mptcp_redundant
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_redundant.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_redundant.ko
 
 #roundrobin
 sudo modprobe -v mptcp_rr
-sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_rr.ko
+#sudo insmod  /lib/modules/$(uname -r)/kernel/net/mptcp/mptcp_rr.ko
 
 
 
